931d3ce6d00c54e0df21555abfaeb707bfb90c30
artemdmitrievich <166545999+artemdmitrievich@users.noreply.github.com>
2024-04-13 07:04:24 +0500

    Add files via upload

---
+++ other_files/goods_list.py
@@ -0,0 +1,74 @@
+goods = [
+    {
+        "image": "deps/images/goods/acc.png",
+        "name": "Клавиатура",
+        "description": "Тактильно приятная клавиатура с быстрым откликом.",
+        "price": 150.00,
+    },
+    {
+        "image": "deps/images/goods/controller.png",
+        "name": "Контроллер",
+        "description": "Контроллер для игр на консолях и пк.",
+        "price": 93.00,
+    },
+    {
+        "image": "deps/images/goods/cpu.png",
+        "name": "Cpu-процессор",
+        "description": "Мощнейший процессор для сложных вычислений.",
+        "price": 670.00,
+    },
+    {
+        "image": "deps/images/goods/gaming%20pc.png",
+        "name": "Игровой компьютер",
+        "description": "Современнейший геймерский пк для тяжелых игр.",
+        "price": 365.00,
+    },
+    {
+        "image": "deps/images/goods/gpu.png",
+        "name": "Видеокарта rtx-4090-super",
+        "description": "Новейшая rtx-4090-super - лучшее вложение в пк для заядлых геймеров на ближайшие 10 лет.",
+        "price": 430.00,
+    },
+    {
+        "image": "deps/images/goods/laptop.png",
+        "name": "Ноутбук aser",
+        "description": "Ноутбук - незаменимая вещь для любого программиста, скорее приобретай лучший из них.",
+        "price": 610.00,
+    },
+    {
+        "image": "deps/images/goods/vr.png",
+        "name": "Vr-гарнитура",
+        "description": "Окунись в мир новых технологий и испытай невероятные ощущения с нашим Vr-комплектом.",
+        "price": 55.00,
+    },
+    {
+        "image": "deps/images/goods/other.png",
+        "name": "Вентилятор для пк",
+        "description": "Охлаждение - неотъемлемая часть любого компьютера, необходимая для долгой работы вашего утройства.",
+        "price": 190.00,
+    },
+    {
+        "image": "deps/images/goods/acc.png",
+        "name": "Клавиатура",
+        "description": "Тактильно приятная клавиатура с быстрым откликом.",
+        "price": 30.00,
+    },
+    {
+        "image": "deps/images/goods/controller.png",
+        "name": "Контроллер",
+        "description": "Контроллер для игр на консолях и пк.",
+        "price": 10.00,
+    },
+    {
+        "image": "deps/images/goods/cpu.png",
+        "name": "Cpu-процессор",
+        "description": "Мощнейший процессор для сложных вычислений.",
+        "price": 15.00,
+    },
+    {
+        "image": "deps/images/goods/gaming%20pc.png",
+        "name": "Игровой компьютер",
+        "description": "Современнейший геймерский пк для тяжелых игр.",
+        "price": 25.00,
+    },
+]
